7158bf8709056d5e8f6cb4426058f7c0d0815970
Kiyumiii <itskiyumi@gmail.com>
2023-03-27 15:33:24 +0200

    Envoi du projet sur ce Github

---
+++ Pendu/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace Pendu
+{
+    partial class fen
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblInstruc = new System.Windows.Forms.Label();
+            this.txtMot = new System.Windows.Forms.TextBox();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.picPendu = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.gpbButtons = new System.Windows.Forms.GroupBox();
+            this.lbLettresTestées = new System.Windows.Forms.Label();
+            this.lblResult = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picPendu)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.gpbButtons.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblInstruc
+            // 
+            this.lblInstruc.AutoSize = true;
+            this.lblInstruc.Location = new System.Drawing.Point(19, 0);
+            this.lblInstruc.Name = "lblInstruc";
+            this.lblInstruc.Size = new System.Drawing.Size(111, 17);
+            this.lblInstruc.TabIndex = 5;
+            this.lblInstruc.Text = "Mot à chercher :";
+            // 
+            // txtMot
+            // 
+            this.txtMot.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMot.Location = new System.Drawing.Point(21, 30);
+            this.txtMot.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtMot.Name = "txtMot";
+            this.txtMot.Size = new System.Drawing.Size(403, 34);
+            this.txtMot.TabIndex = 1;
+            this.txtMot.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txbMot_KeyPress);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(579, 284);
+            this.btnReset.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(101, 43);
+            this.btnReset.TabIndex = 10;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Lettres testées";
+            // 
+            // picPendu
+            // 
+            this.picPendu.Image = global::Pendu.Properties.Resources.pendu0;
+            this.picPendu.Location = new System.Drawing.Point(459, 14);
+            this.picPendu.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.picPendu.Name = "picPendu";
+            this.picPendu.Size = new System.Drawing.Size(309, 265);
+            this.picPendu.TabIndex = 6;
+            this.picPendu.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtMot);
+            this.groupBox1.Controls.Add(this.lblInstruc);
+            this.groupBox1.Location = new System.Drawing.Point(11, 14);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox1.Size = new System.Drawing.Size(431, 79);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            // 
+            // gpbButtons
+            // 
+            this.gpbButtons.Controls.Add(this.lbLettresTestées);
+            this.gpbButtons.Controls.Add(this.label1);
+            this.gpbButtons.Location = new System.Drawing.Point(20, 127);
+            this.gpbButtons.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.gpbButtons.Name = "gpbButtons";
+            this.gpbButtons.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.gpbButtons.Size = new System.Drawing.Size(400, 199);
+            this.gpbButtons.TabIndex = 8;
+            this.gpbButtons.TabStop = false;
+            // 
+            // lbLettresTestées
+            // 
+            this.lbLettresTestées.AutoSize = true;
+            this.lbLettresTestées.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbLettresTestées.Location = new System.Drawing.Point(24, 98);
+            this.lbLettresTestées.Name = "lbLettresTestées";
+            this.lbLettresTestées.Size = new System.Drawing.Size(0, 25);
+            this.lbLettresTestées.TabIndex = 6;
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResult.Location = new System.Drawing.Point(83, 342);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 25);
+            this.lblResult.TabIndex = 9;
+            // 
+            // fen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(807, 409);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.gpbButtons);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.picPendu);
+            this.Controls.Add(this.btnReset);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "fen";
+            this.Text = "Pendu";
+            ((System.ComponentModel.ISupportInitialize)(this.picPendu)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.gpbButtons.ResumeLayout(false);
+            this.gpbButtons.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblInstruc;
+        private System.Windows.Forms.TextBox txtMot;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox picPendu;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox gpbButtons;
+        private System.Windows.Forms.Label lbLettresTestées;
+        private System.Windows.Forms.Label lblResult;
+    }
+}
+
---
+++ Pendu/Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Pendu
+{
+    public partial class fen : Form
+    {
+        string mot = "";
+        int rate = 0;
+        int score = 0;
+        int LIMIT = 15;
+
+        public fen()
+        {
+            InitializeComponent();
+            initialiseBouttonsLettres();
+            txtMot.Focus();
+            LockUnlock(false);
+        }
+
+        /// <summary>
+        /// Initialise les boutons dans la groupbox.
+        /// </summary>
+        private void initialiseBouttonsLettres()
+        {
+            int x = 5; int y = 15; int MARGE = 5; int TAILLE = 30;
+            for (int k = 0; k < 26; k++)
+            {
+                Button btn = new Button();
+                gpbButtons.Controls.Add(btn);
+                btn.Size = new Size(TAILLE, TAILLE);
+                
+                if (x+TAILLE+MARGE > gpbButtons.Width) //400 = largeur de la groupbox. //Saut de ligne
+                {
+                    y += TAILLE + MARGE;
+                    x = 5;
+                }
+                btn.Location = new Point(x, y);
+                btn.Text = ((char)('A' + k)).ToString();
+                btn.Click += new EventHandler(Test_Click);
+                x += TAILLE + MARGE;
+            }
+        }
+
+        /// <summary>
+        /// Vérifie si le charactère entré est dans la limite, et valide le mot entré avec la touche Entrée.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void txbMot_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+            if ((!verifieLettreEntreAZ(e.KeyChar) && e.KeyChar != (char)Keys.Back) 
+                || (txtMot.Text.Length >= LIMIT && e.KeyChar != (char)Keys.Back))
+            {
+                e.Handled = true;
+            }
+
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                mot = txtMot.Text;
+                txtMot.Enabled = false;
+                LockUnlock(true);
+                mot = mot.ToUpper();
+                CacheMot(mot);
+            }
+        }
+
+        /// <summary>
+        /// Vérifie si une lettre est comprise entre A et Z, insensiblement à la casse.
+        /// </summary>
+        /// <param name="lettre">Lettre à vérifier.</param>
+        /// <returns>True si compris entre A et Z (ou a et z), false sinon.</returns>
+        private bool verifieLettreEntreAZ(char lettre)
+        {
+            return ((lettre >= 65) && (lettre <= 90)) || ((lettre >= 97 && lettre <= 122));
+        }
+
+        /// <summary>
+        /// Effectue le test de la lettre cliquée.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Test_Click(object sender, EventArgs e)
+        {
+            Button btnClique = (Button)sender; 
+            
+            bool found = false;
+            // Compare la lettre proposée avec chaque lettre du mot
+            try
+            {
+                found = verifieMot(btnClique);
+                gpbButtons.Controls[0].Focus();
+
+                if (!found) //Raté : Met à jour le dessin 
+                {
+                    rate++;
+                    btnClique.Enabled = false;
+                    string penduImg = "pendu" + rate.ToString();
+                    picPendu.Image = (Image)Properties.Resources.ResourceManager.GetObject(penduImg);
+
+                    if (rate == 10) // Si partie finie
+                    {
+                        lblResult.Text = "Perdu !" ;
+                        // Affiche la bonne réponse à la place des -
+                        txtMot.Text = "";
+                        for (int k = 0; k < mot.Length; k++)
+                        {
+                            txtMot.Text += mot[k] + " ";
+                        }
+                        LockUnlock(false);
+                    }
+                }
+
+                if (score == mot.Length) // Fin de partie
+                {
+                    lblResult.Text = "Gagné !";
+                    LockUnlock(false);
+                    btnReset.Focus();
+                }
+            }
+            catch{}
+
+        }
+
+        /// <summary>
+        /// Vérifie si la lettre cliquée apparaît dans le mot, et met à jour le mot en conséquence.
+        /// </summary>
+        /// <param name="btnClique"></param>
+        /// <returns>True si la lettre a été trouvée, False sinon.</returns>
+        private bool verifieMot(Button btnClique)
+        {
+            bool found = false;
+            for (int k = 0; k != mot.Length; k++)
+            {
+                int index = mot.IndexOf(btnClique.Text, k, 1);
+                if (index != -1) // Remplace les - par la lettre
+                {
+                    txtMot.Text = txtMot.Text.Remove(index * 2, 1); //*2 pour les espaces
+                    txtMot.Text = txtMot.Text.Insert(index * 2, btnClique.Text);
+                    found = true;
+                    btnClique.Enabled = false;
+                    score++;
+                    lblResult.Text = "";
+                }
+            }
+            return found;
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            mot = "";
+            rate = 0;
+            score = 0;
+            picPendu.Image = (Image)Properties.Resources.ResourceManager.GetObject("pendu0");
+            txtMot.Clear();
+            txtMot.Enabled = true;
+            lblResult.Text = "";
+            LockUnlock(false);
+            txtMot.Focus();
+        }
+
+        private void CacheMot(string mot)
+        {
+            txtMot.Text = "";
+            for (int k = 0; k < mot.Length; k++)
+            {
+                txtMot.Text += "- ";
+            }
+        }
+
+        private void LockUnlock(bool unlock)
+        {
+            for (int k = 0; k < gpbButtons.Controls.Count; k++)
+            {
+                gpbButtons.Controls[k].Enabled = unlock;
+            }
+        }
+
+    }
+}
